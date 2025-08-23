611de1e7d41f34381ad80e9faceb7b7223cc7755
Hakan Candar <hakancandar@protonmail.com>
2025-08-24 01:51:11 +0300

    Implement WebAssembly-Host type marshalling helper

---
+++ WasmEdgeSDL_map.c
@@ -0,0 +1,24 @@
+#include "WasmEdgeSDL_map.h"
+#include <stdlib.h>
+
+#define WASMEDGE_SDL_IMPL(TYPE) \
+  static TYPE* WasmEdgeSDL_##TYPE##_table[1024]; \
+  static uint32_t WasmEdgeSDL_##TYPE##_count = 1; /* 0 reserved */ \
+  \
+  int32_t WasmEdgeSDL_Register_##TYPE(TYPE *reg) { \
+    if (WasmEdgeSDL_##TYPE##_count >= 1024) { \
+      return 0; /* table full */ \
+    } \
+    uint32_t id = WasmEdgeSDL_##TYPE##_count++; \
+    WasmEdgeSDL_##TYPE##_table[id] = reg; \
+    return id; \
+  } \
+  \
+  TYPE* WasmEdgeSDL_Recall_##TYPE(int32_t id) { \
+    if (id == 0 || id >= WasmEdgeSDL_##TYPE##_count) { \
+      return NULL; \
+    } \
+    return WasmEdgeSDL_##TYPE##_table[id]; \
+  }
+
+WASMEDGE_SDL_IMPL(SDL_Window)
---
+++ WasmEdgeSDL_map.h
@@ -0,0 +1,24 @@
+#ifndef WASMEDGE_SDL_MAP_H
+#define WASMEDGE_SDL_MAP_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <SDL3/SDL.h>
+
+/*
+ * Macro to generate register/recall functions for a given type.
+ * Example:
+ *   WASMEDGE_SDL_MAP(SDL_Window)
+ *
+ * Expands into:
+ *   uint32_t WasmEdgeSDL_Register_SDL_Window(SDL_Window *reg);
+ *   SDL_Window* WasmEdgeSDL_Recall_SDL_Window(uint32_t id);
+ */
+
+#define WASMEDGE_SDL_MAP(TYPE) \
+  int32_t WasmEdgeSDL_Register_##TYPE(TYPE *reg); \
+  TYPE* WasmEdgeSDL_Recall_##TYPE(int32_t id);
+
+WASMEDGE_SDL_MAP(SDL_Window)
+
+#endif /* WASMEDGE_SDL_MAP_H */
